ab7192deb3b72c1c2c440a5c5b2ed8ea43c59e79
TheOnlyRealRyan <ryansputzier@gmail.com>
2025-02-26 15:41:05 +0200

    renaming folders

---
+++ layout/main.py
@@ -0,0 +1,11 @@
+# Class Here
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        for i in range(n):
+            
+            print(i+1)
+            
+        
+if __name__ == "__main__":
+    n= 2
+    print(Solution.climbStairs(Solution, n))
